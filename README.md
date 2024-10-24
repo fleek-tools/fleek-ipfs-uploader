78b0e24488f40dd6b5f7f78467f2efb787ecd2bd
trachchung <nguyentrachchung@gmail.com>
2024-10-24 20:21:15 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,164 @@
+## Project Description
+
+![Application Screenshot](image.png)
+
+This is a web application using Nextjs, Fleek SDK + CLI, IPFS Storage.
+
+It allows users to upload images to IPFS via Fleek SDK
+
+It is deployed by using fleek-next CLI https://github.com/fleek-platform/fleek-next
+
+```
+The app contains two main parts:
+- Next.js Front-end deployed by fleek-next CLI
+- Node.js Back-end using Fleek SDK to upload images to IPFS
+```
+
+# FE: Nextjs Web
+
+## Get the project id
+
+```bash
+# fleek CLI https://fleek.xyz/docs/cli/
+# fleek login is required
+fleek login
+
+fleek projects create --name fleek-ipfs-uploader
+# ✅ Success! The project "fleek-ipfs-uploader" has been successfully created with the project ID "cm2css5d1000tgqiqz5wd9iaa", and you've automatically been switched to it.
+```
+
+## fleek.json file
+
+create fleek.json and update `FLEEK_PROJECT_ID`
+
+```bash
+# fe/package.json
+{
+    "FLEEK_PROJECT_ID": "cm2css5d1000tgqiqz5wd9iaa"
+}
+```
+
+## Get Fleek Personal Access Token (PAT)
+
+```bash
+fleek pat create
+# 🤖 Please follow the link to log in to Fleek Platform.
+# 🔗 https://app.fleek.xyz/login/<login hash>
+# ✅ Success! Your new personal access token: <pat_hash>
+```
+
+## Update FE .env file
+
+```bash
+# In `fe` working directory
+cp .env.local.example .env.local
+
+# Update following environment variables in `.env`
+# Fleek personal access token
+PAT_TOKEN= <your_pat_token>
+# Fleek project id
+PROJECT_ID= <your_project_id>
+```
+
+## Run the FE on local
+
+```bash
+npm run build
+npm run dev
+```
+
+After running the Nextjs FE on local, the web service runs on [http://localhost:3000](http://localhost:3000)
+
+```
+> fleek-ipfs-uploader@0.1.0 dev
+> next dev
+  ▲ Next.js 14.2.15
+  - Local:        http://localhost:3000
+  - Environments: .env.local
+ ✓ Starting...
+ ✓ Ready in 2.1s
+```
+
+# BE: Nodejs Webserver
+
+### Setup
+
+```
+# Prerequisites
+Node 18+
+Fleek Account
+Fleek CLI
+Fleek SDK Installation
+```
+
+```bash
+# Change current directory to `be`
+cd be
+
+# Install dependencies
+npm i
+
+# Create `.env` file
+cp .env.example .env
+# Update following environment variables in `.env`
+# Fleek personal access token
+PAT_TOKEN=
+# Fleek project id
+PROJECT_ID=
+
+# Start the nodejs server (in `be` working directory)
+node server.js
+# Server is running on http://127.0.0.1:8080
+```
+
+### Ngok installation for Nodejs deployment
+
+[ngrok](https://ngrok.com/download)
+
+[ngrok signup](https://dashboard.ngrok.com/signup)
+
+```bash
+# add auth token to ngrok, can be get here:
+# https://dashboard.ngrok.com/get-started/your-authtoken
+ngrok config add-authtoken <your_ngrok_auth_token>
+#✅ Authtoken saved to configuration file: ~/.config/ngrok/ngrok.yml
+
+# Start the ngrok server
+ngrok http 8080
+
+#forwarding: <ngrok_url_to_the_server> (e.g. https://36b3-104-28-254-75.ngrok-free.app) -> http://localhost:8080
+
+```
+
+### Update BACK_END_URL in `fe` folder
+
+After starting up the ngrok, it returns a http url like [https://36b3-104-28-254-00.ngrok-free.app](https://36b3-104-28-254-00.ngrok-free.app)
+
+Go to `fe/app/page.tsx` and update `BE_SERVER_URL`
+
+```code
+7 const BE_SERVER_URL = <ngrok_url_to_the_server>;
+```
+
+# Deploy Nextjs Web using Fleek-next CLI
+
+```bash
+# fleek-next CLI Installation
+#global installation
+npm i -g @fleek-platform/next
+
+fleek-next deploy
+```
+
+```
+# fleek-next deployment sucess
+✅ Success! Next.js app successfully bundled
+✅ Success! λ Function fleek-ipfs-uploader successfully deployed...
+
+url                                                   type
+---------------------------------------------------------------
+https://hissing-kitchen-harsh.functions.on-fleek.app  functions
+
+✅ Success! Your Next.js app has been successfully deployed to Fleek
+> Visit your app at https://hissing-kitchen-harsh.functions.on-fleek.app
+```
---
+++ fe/app/page.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const BE_SERVER_URL = "<ngrok_url_to_the_server>";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default function HomePage() {
+  const [file, setFile] = useState(null);
+  const [cid, setCid] = useState<string>("");
+  const [uploadPreview, setUploadPreview] = useState("");
+  const [getImagePreview, setGetImagePreview] = useState("");
+  const [cidUploaded, setCidUploaded] = useState<string>("");
+
+  const handleFileChange = (e: any) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+    setUploadPreview(URL.createObjectURL(selectedFile));
+  };
+
+  const handleSubmit = async (e: any) => {
+    try {
+      e.preventDefault();
+      if (!file) return;
+
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await fetch(`${BE_SERVER_URL}/upload`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (res.ok) {
+        console.log("File uploaded successfully!");
+        const responseJson = await res.json();
+        setCidUploaded(responseJson?.data?.pin?.cid);
+      } else {
+        console.log(
+          `Error uploading file: ${res.status} - ${res.statusText}
+          ${await res.json()}`
+        );
+      }
+    } catch (error) {
+      console.log("handleSubmit: Error uploading file.", error);
+    }
+  };
+
+  const handleFetchImage = async () => {
+    try {
+      if (!cid) return;
+      const ipfsRes = await fetch(`https://gateway.pinata.cloud/ipfs/${cid}`);
+      if (ipfsRes.ok) {
+        const blob = await ipfsRes.blob();
+        const imageUrl = URL.createObjectURL(blob);
+        setGetImagePreview(imageUrl);
+      } else {
+        console.log("Error fetching image.");
+      }
+    } catch (error) {
+      console.log("Error fetching image.", error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center p-6 border border-gray-300 rounded-lg max-w-lg mx-auto bg-white shadow-lg">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center space-y-4"
+      >
+        <label
+          htmlFor="file-upload"
+          className="cursor-pointer bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg transition-colors duration-300"
+        >
+          Choose a file
+        </label>
+        <input
+          id="file-upload"
+          type="file"
+          onChange={handleFileChange}
+          className="hidden"
+        />
+        <button
+          type="submit"
+          className="bg-green-500 hover:bg-green-600 text-white py-2 px-6 rounded-lg transition-colors duration-300"
+        >
+          Upload
+        </button>
+        {uploadPreview && (
+          <div className="mt-6 relative h-[250px] w-full">
+            <Image
+              src={uploadPreview}
+              alt="Upload Image Preview"
+              className="w-48 h-48 object-cover rounded-lg shadow-md"
+              fill
+            />
+          </div>
+        )}
+        {cidUploaded && (
+          <p className="mt-4 text-green-600">CID Uploaded: {cidUploaded}</p>
+        )}
+        <input
+          type="text"
+          value={cid}
+          onChange={(e) => setCid(e.target.value)}
+          placeholder="Enter CID hash"
+          className="mb-4 p-2 border border-gray-300 rounded"
+        />
+        <button
+          type="button"
+          onClick={handleFetchImage}
+          className="ursor-pointer  hover:bg-blue-600 text-white py-2 px-4 rounded-lg transition-colors duration-300 mb-4 p-2 bg-blue-500"
+        >
+          Fetch Image
+        </button>
+      </form>
+
+      {getImagePreview && (
+        <div className="mt-6 relative h-[250px] w-full">
+          <Image
+            src={getImagePreview}
+            alt="Upload Image Preview"
+            className="w-48 h-48 object-cover rounded-lg shadow-md"
+            fill
+          />
+        </div>
+      )}
+    </div>
+  );
+}
